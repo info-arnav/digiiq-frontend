8666f82adb99b107019572992c8032aac4951c46
anushajain16062004 <anushajain1606@gmail.com>
2025-05-20 22:26:03 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,21 @@
-// src/components/Navbar.jsx
 import React from 'react';
 import './Navbar.css';
 
 function Navbar({ onGetStarted }) {
   return (
     <nav className="navbar">
-      <div className="logo">LOGO</div>
-      <ul className="nav-links">
-        <li>Models</li>
-        <li>Resources</li>
-        <li>Pricing</li>
-        <li>Products</li>
-      </ul>
-      <button className="get-started" onClick={onGetStarted}>
-        Get Started
-      </button>
+      <div className="navbar-content">
+        <div className="logo">DIGI IQ</div>
+        <ul className="nav-links">
+          <li>Models</li>
+          <li>Resources</li>
+          <li>Pricing</li>
+          <li>Products</li>
+        </ul>
+        <button className="get-started" onClick={onGetStarted}>
+          Get Started
+        </button>
+      </div>
     </nav>
   );
 }
